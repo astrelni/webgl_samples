00e63d6e3637bb47e74bfea17afdd56f4ce7763a
Alex Strelnikov <astrel15@gmail.com>
2020-06-13 21:25:02 -0400

    Just use a global for the context

--- 01_setup/script.js
+++ 01_setup/script.js
@@ -1,11 +1,12 @@
-function getGlContext() {
-  const gl = document.getElementById("gl-canvas").getContext("webgl");
+var gl = null;
+
+function initGL() {
+  gl = document.getElementById("gl-canvas").getContext("webgl");
   if (gl === null) throw "Failed to get GL context";
-  return gl;
 }
 
 function onLoad() {
-  const gl = getGlContext();
+  initGL();
   gl.clearColor(0.0, 1.0, 1.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
